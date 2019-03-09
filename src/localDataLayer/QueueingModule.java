4584d038fba3a5bce37ee02ac34fe8be70954bbe
Ahmad Shaaban <ahmedshaaban.fci@gmail.com>
2019-03-09 23:14:51 +0200

    Add files via upload

--- src/localDataLayer/QueueingModule.java
+++ src/localDataLayer/QueueingModule.java
@@ -1,11 +1,29 @@
 package localDataLayer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import communicationLayer.IQueueingModule;
 
 public class QueueingModule implements IQueueingModule{
 	private DataStore localData;
-	public QueueingModule(DataStore localData)
+	private Queue<String> cache;
+	private int limit;
+	public QueueingModule(DataStore localData,int limit)
 	{
 		this.localData=localData;
+		cache=new LinkedList<>();
+		this.limit=limit;
+	}
+	public boolean insert(String msg)
+	{
+		if(cache.size()>=limit-1)
+			return false;
+		cache.add(msg);
+		return true;
+	}
+	public boolean pushToDataStore(String msg)
+	{
+		return localData.insert(msg);
 	}
 }
