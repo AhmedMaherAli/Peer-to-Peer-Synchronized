da17b419e314f56d7b2a639b1164e6176868bd03
Ahmad Shaaban <ahmedshaaban.fci@gmail.com>
2019-03-10 19:36:11 +0200

    Add files via upload

--- src/communicationLayer/CircuitBreaker.java
+++ src/communicationLayer/CircuitBreaker.java
@@ -2,9 +2,25 @@ package communicationLayer;
 
 public class CircuitBreaker {
 	private IHandlePeer handleNetwork;
+	private boolean status;
 	public CircuitBreaker(IHandlePeer handleNetwork)
 	{
+		status=true;
 		this.handleNetwork=handleNetwork;
+	}
+	public void close()
+	{
+		status=true;
+	}
+	public void open()
+	{
+		handleNetwork.sendBreakAck();
+		handleNetwork.leaveGroup();
+		status=false;
+	}
+	public boolean status()
+	{
+		return status;
 	}
 
 }
