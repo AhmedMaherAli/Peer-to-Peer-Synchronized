ece6912e53a7bbd2a379d52fe7548bfed8d0bcc6
AhmedMaherAli <45319458+AhmedMaherAli@users.noreply.github.com>
2019-03-10 22:00:37 +0200

    Add files via upload
    The cashingThread class

--- src/communicationLayer/CachingThread.java
+++ src/communicationLayer/CachingThread.java
@@ -1,16 +1,27 @@
 package communicationLayer;
 
 public class CachingThread extends Thread{
-	private IHandlePeer handleNetwork;
+	private IQueueingModule cache;
 	private CircuitBreaker circuit;
-	public CachingThread(IHandlePeer handleNetwork,CircuitBreaker circuit)
+	public CachingThread(IQueueingModule cache,CircuitBreaker circuit)
 	{
-		this.handleNetwork=handleNetwork;
+		this.cache=cache;
 		this.circuit=circuit;
 	}
 	@Override
 	public void run()
 	{
-		
+		while(true)
+		{
+			String msg=cache.poll();
+			if(msg!=null)
+			{
+				boolean isStoreNotFull=cache.pushToDataStore(msg);
+				if(!isStoreNotFull)
+				{
+					circuit.open();
+				}
+			}
+		}
 	}
 }
