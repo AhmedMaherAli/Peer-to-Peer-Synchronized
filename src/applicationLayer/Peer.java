afffa053c00625f2bc50f333279da84086d232c6
AhmedMaherAli <45319458+AhmedMaherAli@users.noreply.github.com>
2019-03-10 20:14:30 +0200

    Completing the peer class
    All the needed attributes at peer class are available now

--- src/applicationLayer/Peer.java
+++ src/applicationLayer/Peer.java
@@ -1,5 +1,38 @@
 package applicationLayer;
 
-public class Peer {
+import communicationLayer.CachingThread;
+import communicationLayer.CircuitBreaker;
+import communicationLayer.FakeDataGenerator;
+import communicationLayer.HandlePeer;
+import communicationLayer.IHandlePeer;
+import communicationLayer.IQueueingModule;
+import communicationLayer.ReceivingThread;
+import localDataLayer.DataStore;
+import localDataLayer.QueueingModule;
 
+public class Peer {
+	private DataStore localData;
+	private IHandlePeer handleNetwork;
+	private IQueueingModule queueingModule;
+	private ReceivingThread receiveThread;
+	private CachingThread cacheThread;
+	private CircuitBreaker circuit;
+	private FakeDataGenerator generator;
+	public Peer(int cacheLimit,int dataLimit)
+	{
+		localData=new DataStore(dataLimit);
+		queueingModule=new QueueingModule(localData,cacheLimit);
+		handleNetwork=new HandlePeer(queueingModule);
+		receiveThread=new ReceivingThread(handleNetwork);
+		circuit=new CircuitBreaker(handleNetwork);
+		cacheThread=new CachingThread(queueingModule, circuit);
+		generator=new FakeDataGenerator(handleNetwork, queueingModule);
+	}
+	public void start()
+	{
+		generator.start();
+		receiveThread.start();
+		cacheThread.start();
+	}
+	
 }
